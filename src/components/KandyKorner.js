95880ef3184c9395260cb3a4301e88c7e5e8ad8e
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-21 14:02:55 -0600

    fetched locations from api and deployed app

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { LocationList } from "./locations/LocationList"
+
+export const KandyKorner = () => {
+    return (
+        <>
+            <h1>KandyKorner</h1>
+            <div>
+            <h1>Locations</h1>
+            <LocationList />
+            </div>
+        </>
+    )
+}
